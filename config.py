75e21119024cd222ba4b82ec8396f6e7cdf91a2e
fauzanaditia7 <64374478+fauzanaditia7@users.noreply.github.com>
2020-11-02 16:31:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config:
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("1597604"))
+    API_HASH = os.environ.get("afa675e093d83bd9023b00f766f69caf")
+    AUTH_USERS = set(int(x) for x in os.environ.get("1176240952", "").split())
+    BOT_TOKEN = os.environ.get("1440835989:AAGNNkonAwkSflUc6jYsYZwQC-92HNJ24ok")
     DB_URI = os.environ.get("DATABASE_URL")
     SESSION_NAME = os.environ.get("SESSION_NAME")
     WORKERS = 200
